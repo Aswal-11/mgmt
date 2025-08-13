f4139ddf65419cacce2fd9ce145bf09c97c712fc
Aswal-11 <kartikaswal2001@gmail.com>
2025-08-13 22:59:32 +0530

    Revert "shacn installed"
    This reverts commit e2f77c99e7e77029715a85a665bc76fab2c3571b.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
-    import { defineConfig } from 'vite';
-    import laravel from 'laravel-vite-plugin';
-    import tailwindcss from '@tailwindcss/vite'
-    import react from '@vitejs/plugin-react';
-    import path from 'path'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
 
-    export default defineConfig({
-        plugins: [
-            laravel({
-                input: 'resources/js/app.jsx',
-                refresh: true,
-            }),
-            tailwindcss(),
-            react(),
-        ],
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, 'resources/js'),
-            },
+export default defineConfig({
+    plugins: [
+        laravel({
+            input:  'resources/js/app.jsx',
+            refresh: true,
+        }),
+        tailwindcss(),
+        react(),
+    ],
+    resolve:{
+        alias:{
+            "@":"/resources/js"
         }
-    });
+    }
+});
